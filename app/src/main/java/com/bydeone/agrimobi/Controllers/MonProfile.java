87db725e00c0096ed6903c851fe34fa6884f6d8a
patrick <playbydeone@gmail.com>
2019-04-26 15:09:11 +0100

    Acces à la base d données firebase sur une nouvelle branche Commit

---
+++ app/src/main/java/com/bydeone/agrimobi/Controllers/MonProfile.java
@@ -0,0 +1,15 @@
+package com.bydeone.agrimobi.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bydeone.agrimobi.R;
+
+public class MonProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mon_profile);
+    }
+}
